26945a4c76b666de54ad94d8ad3d5c4c7f2af24c
PutraRaimarDelvin <delvinputra805@gmail.com>
2025-12-21 23:28:26 +0700

    tambah destinasi navigasi untuk entry siswa

---
+++ app/src/main/java/com/example/activity8/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.activity8.uicontroller.route
+
+
+import com.example.activity8.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
