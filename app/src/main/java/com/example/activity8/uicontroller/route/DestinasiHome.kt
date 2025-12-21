bbf3f0ff0f7a7f632994575a22141cabdc44aa95
PutraRaimarDelvin <delvinputra805@gmail.com>
2025-12-21 23:28:54 +0700

    tambah destinasi navigasi halaman home

---
+++ app/src/main/java/com/example/activity8/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.activity8.uicontroller.route
+
+
+import com.example.activity8.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
